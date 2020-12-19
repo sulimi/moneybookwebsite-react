5d8b78ed4269ee875639bc47ba9185d84ee14962
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-19 13:33:51 +0800

    deploy

---
+++ precache-manifest.342e14fce7b4f815ac7bb67e20eeaa5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f185471dcc4276d153a0eba6f0d60a6",
+    "url": "/moneybookwebsite-react/index.html"
+  },
+  {
+    "revision": "f23fc4b1384157548eaa",
+    "url": "/moneybookwebsite-react/static/css/main.c58dd17e.chunk.css"
+  },
+  {
+    "revision": "f70a27e33214ab0a3b9f",
+    "url": "/moneybookwebsite-react/static/js/2.bdd3aa54.chunk.js"
+  },
+  {
+    "revision": "e4e6e48a9ecea75dc1966a170f826888",
+    "url": "/moneybookwebsite-react/static/js/2.bdd3aa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23fc4b1384157548eaa",
+    "url": "/moneybookwebsite-react/static/js/main.f87b5f7d.chunk.js"
+  },
+  {
+    "revision": "16bd64c3649ff8b4480d",
+    "url": "/moneybookwebsite-react/static/js/runtime-main.9eff136e.js"
+  }
+]);
